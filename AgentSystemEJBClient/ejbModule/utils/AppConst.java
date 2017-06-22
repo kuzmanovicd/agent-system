215424f74e44248d5ec45a2745568dbd239ff8ec
KUZMANOVICD\deyan <dejank@uns.ac.rs>
2017-06-22 23:24:09 +0200

    update.

--- AgentSystemEJBClient/ejbModule/utils/AppConst.java
+++ AgentSystemEJBClient/ejbModule/utils/AppConst.java
@@ -6,6 +6,6 @@ public class AppConst {
 	public static final String WAR_NAME = "AgentSystemWAR";
 	public static final String REST_ROOT = "api";
 	public static final String HOST_FILE = "host.json";
-	public static final String COMMUNICATOR_NAME = "rest";
+	public static final String COMMUNICATOR_NAME = "zeromq";
 	public static final int JMS_RESPONSE_TIME = 100;
 }
