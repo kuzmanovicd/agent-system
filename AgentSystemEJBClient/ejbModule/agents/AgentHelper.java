d921336276efaf15147577f8119401efc449a6a9
KUZMANOVICD\deyan <dejank@uns.ac.rs>
2017-06-22 07:34:45 +0200

    Small update.

--- AgentSystemEJBClient/ejbModule/agents/AgentHelper.java
+++ AgentSystemEJBClient/ejbModule/agents/AgentHelper.java
@@ -5,6 +5,7 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import mdb.AgentServicesBeanLocal;
+import utils.AppConst;
 import utils.Log;
 
 public class AgentHelper {
@@ -12,10 +13,21 @@ public class AgentHelper {
 	public static AgentServicesBeanLocal getAgentServices() {
 		try {
 			Context cntx = new InitialContext();
-			String name = "java:global/AgentSystemEAR/AgentSystemEJB/AgentServicesBean!" + AgentServicesBeanLocal.class.getName();
+			String name = "java:global/" + AppConst.APP_NAME + "/" + AppConst.EJB_NAME + "/AgentServicesBean!" + AgentServicesBeanLocal.class.getName();
 			AgentServicesBeanLocal a = (AgentServicesBeanLocal) cntx.lookup(name);
 			return a;
+		} catch (NamingException e) {
+			Log.out("AgentHelper - catchhh " + e.getMessage());
+			return null;
+		}
+	}
 
+	public static AgentManagerLocal getAgentManager() {
+		try {
+			Context cntx = new InitialContext();
+			String name = "java:global/" + AppConst.APP_NAME + "/" + AppConst.EJB_NAME + "/AgentManager!" + AgentManagerLocal.class.getName();
+			AgentManagerLocal a = (AgentManagerLocal) cntx.lookup(name);
+			return a;
 		} catch (NamingException e) {
 			Log.out("AgentHelper - catchhh " + e.getMessage());
 			return null;
